6de072b268bff8020df252a2101730843a10bb67
¨Amairany <amairanyr677@gmail.com>
2022-05-04 11:03:12 -0500

    Estos son mis ejercicios de la secion de control de flujo

---
+++ Ejerciciossignozodiacal/assets/js/SignosZodiacales.js
@@ -0,0 +1,13 @@
+function SignosZodiacales(){
+    console.log("Ingresar dia de nacimiento");
+    console.log("Ingresar mes de nacimiento");
+    let variable = promt();
+}
+
+
+/*
+    var SignosZodiacales = prompt("Ingresa dia de nacimiento", "Ingresa mes de nacimiento");
+    } else if (Dia es = o > que 21 y mes es = que 3)o(Dia es < o = que 20 y mes es igual que 4){ 
+        
+*/
+
